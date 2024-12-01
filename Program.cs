1a552519930a9959024a85833b331a7681ae7111
Edwin Choate <edwin.choate@pm.me>
2024-12-01 16:00:39 -0500

    gets rid of top-level statements in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace jrnl
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+
+        }
+
+    }
+}
