e653ef31dc2a1196a7abbb411c7276c33074653a
Edwin Choate <edwin.choate@pm.me>
2024-12-02 19:28:06 -0500

    implements NewCommand.Execute

--- Commands/NewCommand.cs
+++ Commands/NewCommand.cs
@@ -8,7 +8,32 @@ namespace jrnl
 
         public override void Execute(string[] args)
         {
-            // TODO
+            string title = GetTitleFromUser();
+
+            PrintInstructions();
+
+            string body = Console.ReadLine() ?? "";
+
+            using (var db = new JournalContext())
+            {
+                db.Add(new JournalEntry { 
+                    Title = title,
+                    Body = body,
+                });
+                db.SaveChanges();
+            }
+        }
+
+        private void PrintInstructions ()
+        {
+            Console.WriteLine("Press enter to finish entry\n" +
+                              "Write a journal entry...");
+        }
+
+        private string GetTitleFromUser()
+        {
+            Console.Write("Title (Optional): ");
+            return Console.ReadLine() ?? "";
         }
 
     }
